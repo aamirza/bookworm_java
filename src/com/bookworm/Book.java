99b670cc6f9792fcc65e6b0c4c50a9a819b4a1c7
Aamir Mirza <aamirshahed@hotmail.com>
2019-12-25 15:07:19 -0500

    Create abstract book class, add virtual functions

--- src/com/bookworm/Book.java
+++ src/com/bookworm/Book.java
@@ -1,22 +1,42 @@
 package com.bookworm;
 
-public class Book {
-    private static final int BOOK = 0;
-    private static final int EBOOK = 1;
-    private static final int AUDIOBOOK = 2;
-
-    private final String title;
-    private int format = BOOK;
-    private int pages;
-    private int pagesComplete = 0;
-    private boolean complete = false;
+public class Book extends iBook {
+    private final int pages;
+    private int pagesComplete;
 
 
-    public Book(String title, int format, int pagesComplete, int pages) {
-        this.title = title;
-        this.format = format;
-        this.pagesComplete = pagesComplete;
+    public Book(String title, int pages) {
+        super(title);
         this.pages = pages;
+        this.pagesComplete = 0;
     }
 
+    public Book(String title, int pages, int pagesComplete) {
+        super(title);
+        this.pages = pages;
+        this.pagesComplete = pagesComplete;
+        this.complete = this.isComplete();
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getPagesComplete() {
+        return pagesComplete;
+    }
+
+    public void setPagesComplete(int pagesComplete) {
+        this.pagesComplete = pagesComplete;
+    }
+
+    @Override
+    public boolean isComplete() {
+        return pagesComplete >= pages;
+    }
+
+    @Override
+    public double percentComplete() {
+        return (double)pagesComplete / (double)pages;
+    }
 }
