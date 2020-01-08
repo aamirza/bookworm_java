07696acc9b9c0c588cc34d570367e0d22a4f7719
Aamir Mirza <aamirshahed@hotmail.com>
2020-01-08 14:08:53 -0500

    Created Model class
    Signed-off-by: Aamir Mirza <aamirshahed@hotmail.com>

--- src/com/bookworm/Prompt.java
+++ src/com/bookworm/Prompt.java
@@ -1,8 +1,14 @@
 package com.bookworm;
 
 public class Prompt {
-    // CRUD: Add a book, get list of books, update books, remove books
-    // Also be able to set goal?
-    // Perhaps this shouldn't be called Prompt, but instead have static methods
-    // Data will be saved in JSON
+    private iBook[] books;
+    private int goal = 0;
+
+    public void setGoal() {
+        if (this.goal <= 0) {
+            System.out.printf("You don't have a goal set. ");
+        }
+        System.out.printf("How many books do you want to read this year? >>> ");
+        
+    }
 }
